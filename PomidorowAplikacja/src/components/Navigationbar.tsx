cd3827720a0075654e2799214f3f2d1e08cece21
joastek <joastek@gmail.com>
2023-05-02 22:04:46 +0200

    Added animation to navigate icons

--- PomidorowAplikacja/src/components/Navigationbar.tsx
+++ PomidorowAplikacja/src/components/Navigationbar.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import Settings from "./NavIcons/Settings";
 import Information from "./NavIcons/Information";
 import classes from "./NavigationBar.module.css";
@@ -6,9 +5,7 @@ const NavigationBar = () => {
   return (
     <nav className={classes.nav}>
       <Settings />
-      <div className={classes.box}>
-        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} />
-      </div>
+
       <Information />
     </nav>
   );
