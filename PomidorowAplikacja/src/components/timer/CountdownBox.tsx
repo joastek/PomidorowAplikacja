fb3a7c0b55d0b07143878177ff72d4d20bd7ffa4
joastek <joastek@gmail.com>
2023-05-03 13:43:36 +0200

    Added timer with bacground

--- PomidorowAplikacja/src/components/timer/CountdownBox.tsx
+++ PomidorowAplikacja/src/components/timer/CountdownBox.tsx
@@ -3,7 +3,9 @@ import classes from "./CountdownBox.module.css";
 function CountDownBox() {
   return (
     <div className={classes.box}>
-      <div className={classes.box_1}></div>
+      <div className={classes.box_1}>
+        <div className={classes.text}>25:00</div>
+      </div>
       <div className={classes.box_2}></div>
       <div className={classes.box_3}></div>
     </div>
