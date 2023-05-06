58dad02c21dc87f693e8fc4a4527d40a27f61308
joastek <joastek@gmail.com>
2023-05-06 15:22:48 +0200

    added closing tag to settings

--- PomidorowAplikacja/src/components/pages/Modal.tsx
+++ PomidorowAplikacja/src/components/pages/Modal.tsx
@@ -1,13 +1,16 @@
 import classes from "./Modal.module.css";
-
+import ReactDom from "react-dom";
 const Modal = ({ open, onClose }: any) => {
   if (!open) return null;
-  return (
-    <div className={classes.backdrop}>
+  return ReactDom.createPortal(
+    <>
+      <div className={classes.backdrop} onClick={onClose}></div>
       <div className={classes.modal}>
         <button onClick={onClose}>escape</button>
       </div>
-    </div>
+    </>,
+
+    document.getElementById("overlays") as HTMLInputElement
   );
 };
 
