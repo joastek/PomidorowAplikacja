0c470c4d859b8c617b95ef31ec99edba0784b4d2
joastek <joastek@gmail.com>
2023-05-06 16:29:22 +0200

    footer updated

--- PomidorowAplikacja/src/components/footer/Footer.tsx
+++ PomidorowAplikacja/src/components/footer/Footer.tsx
@@ -1,35 +1,24 @@
 import classes from "./Footer.module.css";
-
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className={classes.container}>
-      {/* <li>
-        {" "}
-        <a
-          className={classes.icon}
-          href="https://www.facebook.com/jan.czyszczon.31"
-        >
-          <i className="fa-brands fa-facebook"></i>
-        </a>
-      </li>
-      <li>
-        {" "}
-        <a className={classes.icon} href="https://chat.openai.com/">
-          <i className="fa-solid fa-envelope"></i>
-        </a>
-      </li>
-      <li>
-        {" "}
-        <a className={classes.icon} href="https://chat.openai.com/">
-          <i className="fa-brands fa-github"></i>
-        </a>
-      </li>
-      <li>
-        {" "}
-        <a className={classes.icon} href="https://chat.openai.com/">
-          <i className="fa-brands fa-linkedin"></i>
-        </a>
-      </li> */}
+      {" "}
+      <Link
+        className={classes.icon}
+        to="https://www.facebook.com/jan.czyszczon.31"
+      >
+        <i className="fa-brands fa-facebook"></i>
+      </Link>{" "}
+      <Link className={classes.icon} to="https://chat.openai.com/">
+        <i className="fa-solid fa-envelope"></i>
+      </Link>{" "}
+      <Link className={classes.icon} href="https://chat.openai.com/">
+        <i className="fa-brands fa-github"></i>
+      </Link>{" "}
+      <Link className={classes.icon} href="https://chat.openai.com/">
+        <i className="fa-brands fa-linkedin"></i>
+      </Link>
     </div>
   );
 };
--- PomidorowAplikacja/src/components/timer/ActionButtons.tsx
+++ PomidorowAplikacja/src/components/timer/ActionButtons.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import classes from "./ActionButtons.module.css";
 function ActionButtons() {
+  const [time, setTime] = useState(25);
+  const [timer, SetTimer] = useState(false);
+
   return (
-    <div className={classes.box}>
+    <div className={classes.box} onClick={() => SetTimer(true)}>
       <div className={classes.container}>
         {" "}
         <p>Start</p>
       </div>
       <div className={classes.container}>
-        <p>Zatrzymaj</p>
+        <p>Reset</p>
       </div>
     </div>
   );
