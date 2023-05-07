f8048b214fd1f2dd95d016a486249ad2e0366cc5
joastek <joastek@gmail.com>
2023-05-07 21:43:16 +0200

    clock logic added

--- PomidorowAplikacja/src/components/timer/ActionButtons.tsx
+++ PomidorowAplikacja/src/components/timer/ActionButtons.tsx
@@ -1,50 +1,59 @@
-import { useState, useEffect } from "react";
+import { useState, useRef } from "react";
 import classes from "./ActionButtons.module.css";
 import CountDownBox from "./CountdownBox";
-function ActionButtons() {
-  const [time, setTime] = useState(0);
+const ActionButtons: React.FC = () => {
+  const [time, setTime] = useState(60 * 25);
   const [isRunning, setIsRunning] = useState(false);
-  useEffect(() => {
-    setTimeout(() => {
-      setTime(time - 1000);
-    }, 1000);
-  }, [time]);
+  const intervalRef = useRef<number | null>(null);
+
   const startTimer = () => {
     setIsRunning(true);
+    intervalRef.current = window.setInterval(() => {
+      setTime((time) => time - 1);
+    }, 1000);
   };
 
   const stopTimer = () => {
     setIsRunning(false);
+    window.clearInterval(intervalRef.current!);
   };
 
   const resetTimer = () => {
-    setTime(0);
+    setTime(60 * 25);
     setIsRunning(false);
+    window.clearInterval(intervalRef.current!);
   };
-  const getFormattedTime = (milisecond) => {
-    let total_seconds = parseInt(Math.floor(milisecond / 1000));
-    let total_minutes = parseInt(Math.floor(milisecond / 60));
-    let seconds = parseInt(total_seconds % 60);
-    let minutes = parseInt(total_minutes % 60);
-    return `${minutes}${seconds}`;
+
+  const handleStartStopClick = () => {
+    if (isRunning) {
+      stopTimer();
+    } else {
+      startTimer();
+    }
   };
+
+  const handleResetClick = () => {
+    resetTimer();
+  };
+
   return (
     <>
       <CountDownBox time={time} />
+
       <div className={classes.box}>
         <div className={classes.container}>
           {!isRunning ? (
-            <p onClick={startTimer}> Start</p>
+            <p onClick={handleStartStopClick}> Start</p>
           ) : (
-            <p onClick={stopTimer}> Stop</p>
+            <p onClick={handleStartStopClick}> Stop</p>
           )}
         </div>
         <div className={classes.container}>
-          <p onClick={resetTimer}>Reset{getFormattedTime(time)}</p>
+          <p onClick={handleResetClick}>Reset</p>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default ActionButtons;
--- PomidorowAplikacja/src/components/timer/CountdownBox.tsx
+++ PomidorowAplikacja/src/components/timer/CountdownBox.tsx
@@ -1,11 +1,18 @@
 import classes from "./CountdownBox.module.css";
 
 const CountDownBox = ({ time }: any) => {
+  const formatTime = (seconds: number): string => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    const paddedMinutes = String(minutes).padStart(2, "0");
+    const paddedSeconds = String(remainingSeconds).padStart(2, "0");
+    return `${paddedMinutes}:${paddedSeconds}`;
+  };
   return (
     <>
       <div className={classes.container}>
         <div className={classes.box_1}>
-          <div className={classes.text}>:</div>
+          <div className={classes.text}>{formatTime(time)}</div>
         </div>
         <div className={classes.box_2}></div>
         <div className={classes.box_3}></div>
