e53e21223cb44c42520be998512fb4e6d47eb493
joastek <joastek@gmail.com>
2023-05-11 22:26:55 +0200

    initial commit

--- PomidorowAplikacja/src/components/pages/MainPage.tsx
+++ PomidorowAplikacja/src/components/pages/MainPage.tsx
@@ -1,7 +1,5 @@
 import Footer from "../footer/Footer";
 import ActionButtons from "../timer/ActionButtons";
-import CountDownBox from "../timer/CountdownBox";
-import Modal from "../pages/Modal";
 
 const MainPage = () => {
   return (
