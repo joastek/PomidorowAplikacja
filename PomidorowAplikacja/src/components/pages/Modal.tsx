fee3ee1886d60ffb64348af1863c513da3d835c9
joastek <joastek@gmail.com>
2023-05-16 23:15:30 +0200

    replacing svg files

--- PomidorowAplikacja/src/components/pages/Modal.tsx
+++ PomidorowAplikacja/src/components/pages/Modal.tsx
@@ -1,5 +1,6 @@
 import classes from "./Modal.module.css";
 import ReactDom from "react-dom";
+import logo from "../icons/ClockCircleOutlined.png";
 const Modal = ({ open, onClose, handleWorkTimeChange }: any) => {
   if (!open) return null;
   return ReactDom.createPortal(
@@ -7,13 +8,12 @@ const Modal = ({ open, onClose, handleWorkTimeChange }: any) => {
       <div className={classes.backdrop} onClick={onClose}></div>
       <div className={classes.modal}>
         <h1>Ustawienia</h1>
-
-        <input
-          type="text"
-          onChange={handleWorkTimeChange}
-          className={classes.box}
-        ></input>
-        <button onClick={onClose}>escape</button>
+        <div>
+          <img className={classes.logo} src={logo} />
+          <div>Czas Pracy</div>
+        </div>
+        <input type="number" onChange={handleWorkTimeChange} max={99}></input>
+        <button onClick={onClose}>Zatwierdź</button>
       </div>
     </>,
 
