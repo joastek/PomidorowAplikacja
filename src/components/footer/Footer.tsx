4fa7720d890bdd08687654f998dfdae4e269b66e
joastek <joastek@gmail.com>
2023-05-25 22:42:25 +0200

    i had few problems with sscss, i needed to unsitall some packs from extenctions, problem sovled

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,22 +1,19 @@
-import classes from "./Footer.module.css";
+import "./Test.scss";
 import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div className={classes.container}>
+    <div className="container">
       {" "}
-      <Link
-        className={classes.icon}
-        to="https://www.facebook.com/jan.czyszczon.31"
-      >
+      <Link className="icon" to="https://www.facebook.com/jan.czyszczon.31">
         <i className="fa-brands fa-facebook"></i>
       </Link>{" "}
-      <Link className={classes.icon} to="https://chat.openai.com/">
+      <Link className="icon" to="https://chat.openai.com/">
         <i className="fa-solid fa-envelope"></i>
       </Link>{" "}
-      <Link className={classes.icon} href="https://chat.openai.com/">
+      <Link className="icon" to="https://chat.openai.com/">
         <i className="fa-brands fa-github"></i>
       </Link>{" "}
-      <Link className={classes.icon} href="https://chat.openai.com/">
+      <Link className="icon" to="https://chat.openai.com/">
         <i className="fa-brands fa-linkedin"></i>
       </Link>
     </div>
